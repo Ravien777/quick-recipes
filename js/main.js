9133c5a67426dc2cec43b54bf1b0629e5635e4bd
Ravien777 <r.sewpal@unasat.sr>
2023-01-13 06:58:06 -0300

    Card flipping hamburger menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const open_menu = document.querySelector(".open-menu");
+const close_menu = document.querySelector(".close-menu");
+open_menu.addEventListener("click", function () {
+    container.classList.add("is-flipped");
+});
+
+close_menu.addEventListener("click", function () {
+    container.classList.remove("is-flipped");
+});
