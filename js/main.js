9597a2c9618e5abf5b6d04f8a5fc27fb0e30e504
Ravien777 <r.sewpal@unasat.sr>
2023-01-17 09:15:14 -0300

    Add hero img

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ const container = document.querySelector(".container");
 const open_menu = document.querySelector(".open-menu");
 const close_menu = document.querySelector(".close-menu");
 open_menu.addEventListener("click", function () {
-    container.classList.add("is-flipped");
+  container.classList.add("is-flipped");
 });
 
 close_menu.addEventListener("click", function () {
-    container.classList.remove("is-flipped");
+  container.classList.remove("is-flipped");
 });
-
