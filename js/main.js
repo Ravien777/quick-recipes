ea752d9412157f8f6a6263701d225796bd8b2c28
Ravien777 <r.sewpal@unasat.sr>
2023-01-19 01:35:02 -0300

    Close menu button fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const container = document.querySelector(".container");
 const open_menu = document.querySelector(".open-menu");
 const close_menu = document.querySelector(".close-menu");
+const front_card = document.querySelector(".card-front");
+const back_card = document.querySelector(".card-back");
 open_menu.addEventListener("click", function () {
   container.classList.add("is-flipped");
+  disableScroll();
 });
 
 close_menu.addEventListener("click", function () {
   container.classList.remove("is-flipped");
+  enableScroll();
 });
