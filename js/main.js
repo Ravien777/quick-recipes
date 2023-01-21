33c80f9207da4f63b9f28398d3a69479f7b66973
Ravien777 <r.sewpal@unasat.sr>
2023-01-20 22:12:09 -0300

    New section

--- js/main.js
+++ js/main.js
@@ -5,10 +5,8 @@ const front_card = document.querySelector(".card-front");
 const back_card = document.querySelector(".card-back");
 open_menu.addEventListener("click", function () {
   container.classList.add("is-flipped");
-  disableScroll();
 });
 
 close_menu.addEventListener("click", function () {
   container.classList.remove("is-flipped");
-  enableScroll();
 });
