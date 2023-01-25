1c4c96431240b7d159fb53ea74feadc61f231b35
Ravien777 <r.sewpal@unasat.sr>
2023-01-25 13:11:53 -0300

    Menu scrolling test

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
-// const container = document.querySelector(".container");
-// const open_menu = document.querySelector(".open-menu");
-// const close_menu = document.querySelector(".close-menu");
-// const front_card = document.querySelector(".card-front");
-// const back_card = document.querySelector(".card-back");
-// open_menu.addEventListener("click", function () {
-//   container.classList.add("is-flipped");
-// });
-
-// close_menu.addEventListener("click", function () {
-//   container.classList.remove("is-flipped");
-// });
-
 const mobile_menu = document.querySelector(".header-secondary");
 const header_right = document.querySelector(".header-main .right");
 const menu_btn = document.getElementById("toggle-mobile-menu");
+const scrollY = document.body.style.top;
+let menuOpen = false;
 
 document.getElementById("search_form_div").addEventListener("click", () => {
   document.getElementById("search_recipes").focus();
@@ -23,4 +12,13 @@ menu_btn.addEventListener("click", () => {
   menu_btn.classList.toggle("open");
   mobile_menu.classList.toggle("is-active");
   header_right.classList.toggle("not-visible");
+  menuOpen = !menuOpen;
+  if (menuOpen) {
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${window.scrollY}px`;
+  } else {
+    document.body.style.position = "";
+    document.body.style.top = "";
+    window.scrollTo(0, parseInt(scrollY || "0") * -1);
+  }
 });
