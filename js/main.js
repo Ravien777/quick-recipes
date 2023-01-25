ed058f30c3577fd2725f591a524817153094284e
Ravien777 <r.sewpal@unasat.sr>
2023-01-25 10:38:18 -0300

    Mobile home page v1 done

--- js/main.js
+++ js/main.js
@@ -11,7 +11,16 @@
 //   container.classList.remove("is-flipped");
 // });
 
+const mobile_menu = document.querySelector(".header-secondary");
+const header_right = document.querySelector(".header-main .right");
+const menu_btn = document.getElementById("toggle-mobile-menu");
+
 document.getElementById("search_form_div").addEventListener("click", () => {
-  console.log("Clicked");
   document.getElementById("search_recipes").focus();
+});
+
+menu_btn.addEventListener("click", () => {
+  menu_btn.classList.toggle("open");
+  mobile_menu.classList.toggle("is-active");
+  header_right.classList.toggle("not-visible");
 });
